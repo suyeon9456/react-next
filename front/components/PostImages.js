48e6dc1fd3dba2c8b1a2223089c753366f57250d
suyeon <suyeon9456@naver.com>
2021-06-11 16:40:25 +0900

    이미지 구현하기

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
+import { PlusOutlined } from '@ant-design/icons';
 
-const PostImages = () => {
+const PostImages = ({ images }) => {
+  const [showImagesZoom, setShowImageZoom] = useState(false);
+  const onZoom = useCallback(() => {
+    setShowImageZoom(true);
+  });
+
+  if (images.length === 1) {
+    return <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+  };
+
+  if (images.length === 2) {
+    return (
+      <>
+        <img role="presentation" width="50%" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" width="50%" src={images[1].src} alt={images[1].src} onClick={onZoom} />
+      </>
+    )
+  };
   return (
-    <div>구현중</div>
+    <>
+      <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={images[0].src} alt={images[0].src} onClick={onZoom} />
+      <div
+        role="presentation"
+        style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
+        onClick={onZoom}
+      >
+        <PlusOutlined />
+        <br />
+        {images.length - 1}
+        개의 사진 더보기
+      </div>
+    </>
   )
+};
+
+PostImages.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default PostImages;
