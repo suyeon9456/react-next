a4b8894629c1db448644c19470b8fcd8a90dd141
suyeon <suyeon9456@naver.com>
2021-06-18 12:26:22 +0900

    profile 페이지 수정

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
+import Router from 'next/router';
 import { useSelector } from 'react-redux';
 import AppLayout from '../components/AppLayout';
 import NicknameEditForm from '../components/NicknameEditForm';
 import FollowList from '../components/FollowList';
 
 const Profile = () => {
-  const { Followings, Followers } = useSelector((state) => state.user.me);
-
+  const { me } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push('/');
+    }
+  }, [me && me.id]);
+  if (!me) {
+    return null;
+  }
   return (
     <>
       <Head>
@@ -15,8 +23,8 @@ const Profile = () => {
       </Head>
       <AppLayout>
         <NicknameEditForm />
-        <FollowList header="FOLLOWING LIST" data={Followings} />
-        <FollowList header="FOLLOWER LIST" data={Followers} />
+        <FollowList header="FOLLOWING LIST" data={me.Followings} />
+        <FollowList header="FOLLOWER LIST" data={me.Followers} />
       </AppLayout>
     </>
   );
