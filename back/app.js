97b239e82bd1a62cd8605607b2d5e1f7364187a5
suyeon <suyeon9456@naver.com>
2021-06-21 18:09:31 +0900

    express 로 라우팅 하기

--- back/app.js
+++ back/app.js
@@ -1,11 +1,28 @@
-const http = require('http');
-const server = http.createServer((req, res)=> {
-  console.log(req.url, req.method);
-  res.write('node Server1');
-  res.write('node Server2');
-  res.end('node server5');
+const express = require('express'); // 노드가 서버가 아니라 노드에서 제공하는 http 모듈이 서버
+// const server = http.createServer((req, res)=> {
+//   console.log(req.url, req.method);
+//   res.write('node Server1');
+//   res.write('node Server2');
+//   res.end('node server5');
+// });
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello node');
 });
 
-server.listen(3065, () => {
+app.get('/api', (req, res) => {
+  res.send('hello api');
+});
+
+app.get('/post', (req, res) => {
+  res.json([
+    { id: 1, content: 'test1' },
+    { id: 2, content: 'test2' },
+    { id: 3, content: 'test3' }
+  ])
+})
+
+app.listen(3065, () => {
   console.log('서버 실행중');
 });
