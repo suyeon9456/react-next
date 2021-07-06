b530decc268ff8a455bf7e5b8482b86bac266c6a
suyeon <suyeon9456@naver.com>
2021-07-06 19:53:32 +0900

    다이나믹 라우팅

--- front/pages/post/[id].js
+++ front/pages/post/[id].js
@@ -1,5 +1,6 @@
 import React from 'react';
-// import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import { END } from 'redux-saga';
@@ -10,11 +11,23 @@ import AppLayout from '../../components/AppLayout';
 import PostCard from '../../components/PostCard';
 
 const Post = () => {
-  // const router = useRouter();
-  // const { id } = router.query;
+  const router = useRouter();
+  const { id } = router.query;
   const { singlePost } = useSelector((state) => state.post);
   return (
     <AppLayout>
+      <Head>
+        <title>
+          {singlePost.User.nickname} 의 게시물
+        </title>
+        <meta name="description" content={singlePost.content} />
+        {/* url 링크 공유시 아래 아래 설정에 따라 보내짐 */}
+        <meta property="og:title" content={`${singlePost.User.nickname}님의 게시글`} />
+        <meta property="og:description" content={singlePost.content} />
+        {/* favicon.ico 은 public폴더안에 png 파일로 넣어준다. */}
+        <meta property="og:image" content={singlePost.Images[0] ? singlePost.Images[0].src : 'https://nodebird.com/favicon.ico'} />
+        <meta property="og:url" content={`https://nodebird.com/post/${id}`} />
+      </Head>
       <PostCard post={singlePost} />
     </AppLayout>
   );
